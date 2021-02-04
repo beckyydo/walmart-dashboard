e72ab7a1b77daf41c51e24492a6c220ac14d8987
haniehbabaee <70447525+haniehbabaee@users.noreply.github.com>
2021-02-04 17:53:55 -0500

    update

--- Stock/static/js/plot.js
+++ Stock/static/js/plot.js
@@ -1,47 +1,88 @@
-d3.csv("assets/data/WMT.csv").then(function(data){
-    console.log(data);
-    var dates= data.map(record=>record.Date);
-    var closingPrices = data.map(record=>record.Close);
-    var highPrices = data.map(record=>record.High);
-    var lowPrices = data.map(record=>record.Low);
-    var openingPrices = data.map(record=>record.Open);
-    //console.log(openingPrices);
-    
-    var trace1 = {
-        type: "scatter",
-        mode: "lines",
-        name: "Walmart closing prices",
-        x: dates,
-        y: closingPrices,
-        line: {
-          color: "#2e7b71"
-        }
-      };
-  
-      // Candlestick Trace
-      var trace2 = {
-        type: "candlestick",
-        name: "Walmart high, low, open, close stock prices", 
-        x: dates,
-        high: highPrices,
-        low: lowPrices,
-        open: openingPrices,
-        close: closingPrices
-      };
-  
-      var data = [trace1, trace2];
-  
-       var layout = {
-         title: "Walmart Stock",
-         xaxis: {
-           //range: ["1990-01-01", "2020-08-01"],
-           type: "date"
-         },
-         yaxis: {
-           autorange: true,
-           type: "linear"
-         }
-        }
-  
-      Plotly.newPlot("plot", data, layout);
+d3.csv("assets/data/WalmartStock.csv").then(function(data){
+  //console.log(data);
+  var dates= data.map(record=>record.Date);
+  var closingPrices = data.map(record=>record.Close);
+  var highPrices = data.map(record=>record.High);
+  var lowPrices = data.map(record=>record.Low);
+  var openingPrices = data.map(record=>record.Open);
+  var volume= data.map(record=> parseInt(record.Volume))
+  var movingAvg= data.map(record=>record.MovingAvg);
+  var colors = data.map(record=>record.Color)
+  //console.log(movingAvg);
+  //console.log(volume);
+  //console.log(color);
+  trace1 = {
+      name: 'Walmart high, low, open, close stock prices', 
+      type: 'candlestick', 
+      x: dates, 
+      yaxis: 'y2', 
+      low: lowPrices, 
+      high: highPrices, 
+      open: openingPrices, 
+      close: closingPrices
+    };
+    trace2 = {
+      line: {width: 1}, 
+      mode: 'lines', 
+      name: 'Moving Average', 
+      type: 'scatter', 
+      x: dates, 
+      y: movingAvg, 
+      yaxis: 'y2', 
+      marker: {color: '#86797a'}
+    };
+    trace3 = {
+      name: 'Volume', 
+      type: 'bar', 
+      x: dates, 
+      y: volume, 
+      yaxis: 'y', 
+      marker: {
+        color: colors
+      }
+    };
+  var selectorOptions = {
+      buttons: [{
+          step: 'month',
+          stepmode: 'backward',
+          count: 1,
+          label: '1m'
+      }, {
+          step: 'month',
+          stepmode: 'backward',
+          count: 6,
+          label: '6m'
+      }, {
+          step: 'year',
+          stepmode: 'todate',
+          count: 1,
+          label: 'YTD'
+      }, {
+          step: 'year',
+          stepmode: 'backward',
+          count: 1,
+          label: '1y'
+      }, {
+          step: 'all',
+      }],
+  };
+    data = [trace1, trace2, trace3];
+    layout = {
+      title: "Walmart Stock",
+      xaxis: {
+          rangeselector: selectorOptions,
+          rangeslider: {},
+      },
+      yaxis: {
+        domain: [0, 0.2], 
+        showticklabels: false
+      },  
+      yaxis2: {domain: [0.2, 1]}
+   
+    };
+    Plotly.plot('plot', {
+      data: data,
+      layout: layout
+  });
+
 })
