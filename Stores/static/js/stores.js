24e55efb90465d454dd74583f2079e6b596cce75
maylacdao <may.lacdao@gmail.com>
2021-02-05 14:44:22 -0500

    Updates

--- Stores/static/js/stores.js
+++ Stores/static/js/stores.js
@@ -1,4 +1,8 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//Map and tiles
+var mymap = L.map('mapid', {
+    center: [36.334474208699255, -94.12507100205349],
+    zoom: 13
+})
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -8,3 +12,47 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: API_KEY
 }).addTo(mymap);
+
+var marker = L.marker([36.334474208699255, -94.12507100205349]).addTo(mymap);
+
+// Establish data source
+
+// const url = "/api/store";
+
+// var data = d3.json("data/walmart.json").then(function(data) {
+//     console.log(data);
+    
+// });
+
+// ;
+
+// // for (var i = 0; i < locations.length; i++) {
+// //     storeLocations.push(
+// //         L.marker(locations[i].
+// //         )
+// // }
+
+
+// const url = "/api/store";
+// d3.json(url, function(response) {
+//     console.log(response);
+
+//     var storeLocations = response;
+
+//     for (var i = 0; i < storeLocations.length; i++) {
+//         var location = storeLocations[i];
+//         L.marker(storeLocations)
+//             .addTo(mymap);
+//     }
+
+
+// });
+
+var data = d3.json("data/walmart.json").then(function(data) {
+    console.log(data);
+
+    var locationDict = {};
+    locationDict[new_key] = new_value;
+});
+
+
---
+++ Stores/stores.py
@@ -0,0 +1,51 @@
+#################################################
+# Import Dependencies
+#################################################
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine(
+    "postgres://ofiglsqd:vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe@otto.db.elephantsql.com:5432/ofiglsqd", echo=False)
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+store = Base.classes.store
+session = Session(engine)
+
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/api/store")
+def store_route():
+    locationData = session.query(store.latitude, store.longitude).all()
+    session.close()
+    return jsonify(locationData)
+
+
+if __name__ == "__main__":
+    app.run()
