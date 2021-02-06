8793ccbf805ac16fc14ce5c9d6b5087919da25ab
Ronald Clarke <ronnie.clarke@utoronto.ca>
2021-02-05 19:35:15 -0500

    update fri

--- FactorCharts/app.js
+++ FactorCharts/app.js
@@ -1,131 +1,146 @@
-d3.csv("walmartdata.csv").then(function(walData, err) {
-  if (err) throw err;
+var url = "/api/walmart"
 
-  var svgWidth = 960;
-  var svgHeight = 500;
-
-  var margin = {
-    top: 20,
-    right: 40,
-    bottom: 120,
-    left: 100
-  };
-
-  var width = svgWidth - margin.left - margin.right;
-  var height = svgHeight - margin.top - margin.bottom;
-
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-  var svg = d3
-    .select(".chart")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
-
-// Append an SVG group
-  var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Initial Params
-  var chosenXAxis = "Fuel_Price";
-
-// function used for updating x-scale var upon click on axis label
-  function xScale(walData, chosenXAxis) {
-  // create scales
-    var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(walData, d => d[chosenXAxis]),
-        d3.max(walData, d => d[chosenXAxis])
-      ])
-      .range([0, width]);
-
-    return xLinearScale;
-
-  }
-
-// function used for updating xAxis var upon click on axis label
-  function renderAxes(newXScale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXScale);
-
-    xAxis.transition()
-      .duration(1000)
-      .call(bottomAxis);
-
-    return xAxis;
-  }
-
-// function used for updating circles group with a transition to
-// new circles
-  function renderCircles(circlesGroup, newXScale, chosenXAxis) {
-
-    circlesGroup.transition()
-      .duration(1000)
-      .attr("cx", d => newXScale(d[chosenXAxis]));
-
-    return circlesGroup;
-  }
-
-// function used for updating circles group with new tooltip
-  function updateToolTip(chosenXAxis, circlesGroup) {
-
-    var label;
-
-    if (chosenXAxis === "Fuel_Price") {
-      label = "Fuel Price:";
-    }
-    else if (chosenXAxis === "CPI") {
-      label = "CPI:";
-    }
-    else if (chosenXAxis === "Temperature_C") {
-      label = "Temperature C:"
-    }
-    else {
-      label = "Unemployment:"
-    }
-
-    var toolTip = d3.tip()
-      .attr("class", "tooltip")
-      .offset([80, -60])
-      .html(function(d) {
-        return (`Week Date: ${d.Week_Date}<br>${label} ${d[chosenXAxis]}<br>Weekly Sale: $${(d.Weekly_Sales).toFixed(2)}<br>${d.Holiday_Name}`);
-      });
-
-    circlesGroup.call(toolTip);
-
-    circlesGroup.on("mouseover", function(data) {
-      toolTip.show(data);
-    })
-    // onmouseout event
-      .on("mouseout", function(data, index) {
-        toolTip.hide(data);
-      });
-
-    return circlesGroup;
-  }
-
-// Retrieve data from the CSV file and execute everything below
-
-
-  // parse data
-
-  d3.select("#selDataset").selectAll("option")
-    .data(d3.map(walData, function(d){return d.Store;}).keys())
-    .enter()
-    .append("option")
-    .text(function(d){return d;})
-    .attr("value",function(d){return d;});
-
-
-  function getChart(storeID) {
-    walData = walData.filter(d => d.Store === storeID) 
-  }  
-  walData.forEach(function(sample) {
+d3.csv("walmartdata.csv").then(wmartData => {
+  //if (err) throw err;
+  wmartData.forEach(function(sample) {
     sample.Fuel_Price = +sample.Fuel_Price;
     sample.Temperature_C = +sample.Temperature_C;
     sample.Unemployment = +sample.Unemployment;
     sample.Weekly_Sales = +sample.Weekly_Sales;
-    sample.CPI = +sample.CPI
-  });
+    sample.CPI = +sample.CPI;
+    sample.Store = +sample.Store
+  }); 
+  d3.select("#selDataset").selectAll("option")
+    .data(d3.map(wmartData, function(d){return d.Store;}).keys())
+    .enter()
+    .append("option")
+    .text(function(d){return d;})
+    .attr("value",function(d){return d;});
+  d3.select("#selDataset")
+    .append("option")
+    .text("All Stores"); 
+  init(wmartData);
+})
 
+d3.selectAll("#selDataset").on('change', updateChart);
+
+function updateChart() {
+  d3.csv("walmartdata.csv"). then(data => {
+    var dropDown = d3.select("#selDataset").node().value;
+    console.log(dropDown)
+    d3.select(".chart").html("");
+    var updateData;
+    if (dropDown === "All Stores") {
+      init(data)
+    }
+    else {
+      updateData = data.filter(row => row.Store === dropDown)
+      init(updateData)
+    }   
+  })
+}
+
+function init(walData) {
+    var svgWidth = 960;
+    var svgHeight = 500;
+
+    var margin = {
+    top: 20,
+    right: 40,
+    bottom: 120,
+    left: 100
+    };
+
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+
+    // Create an SVG wrapper, append an SVG group that will hold our chart,
+    // and shift the latter by left and top margins.
+    var svg = d3
+        .select(".chart")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+
+    // Append an SVG group
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Initial Params
+    var chosenXAxis = "Fuel_Price";
+
+    // function used for updating x-scale var upon click on axis label
+
+
+    // function used for updating xAxis var upon click on axis label
+    function renderAxes(newXScale, xAxis) {
+        var bottomAxis = d3.axisBottom(newXScale);
+
+        xAxis.transition()
+            .duration(1000)
+            .call(bottomAxis);
+
+        return xAxis;
+    }
+
+    // function used for updating circles group with a transition to
+    // new circles
+    function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+
+        circlesGroup.transition()
+            .duration(1000)
+            .attr("cx", d => newXScale(d[chosenXAxis]));
+
+        return circlesGroup;
+    }
+    function updateToolTip(chosenXAxis, circlesGroup) {
+
+        var label;
+
+        if (chosenXAxis === "Fuel_Price") {
+            label = "Fuel Price:";
+        }
+        else if (chosenXAxis === "CPI") {
+            label = "CPI:";
+        }
+        else if (chosenXAxis === "Temperature_C") {
+            label = "Temperature C:"
+        }
+        else {
+            label = "Unemployment:"
+        }
+
+        var toolTip = d3.tip()
+            .attr("class", "tooltip")
+            .offset([80, -60])
+            .html(function(d) {
+            return (`Week Date: ${d.Week_Date}<br>${label} ${d[chosenXAxis]}<br>Weekly Sale: $${(d.Weekly_Sales)}<br>${d.Holiday_Name}`);
+            });
+
+        circlesGroup.call(toolTip);
+
+        circlesGroup.on("mouseover", function(data) {
+            toolTip.show(data);
+        })
+        // onmouseout event
+            .on("mouseout", function(data, index) {
+            toolTip.hide(data);
+            });
+
+        return circlesGroup;
+    }
+
+    function xScale(walData, chosenXAxis) {
+      // create scales
+        var xLinearScale = d3.scaleLinear()
+          .domain([d3.min(walData, d => d[chosenXAxis]),
+            d3.max(walData, d => d[chosenXAxis])
+          ])
+          .range([0, width]);
+    
+        return xLinearScale;
+    
+      }
   // xLinearScale function above csv import
   var xLinearScale = xScale(walData, chosenXAxis);
 
@@ -292,6 +307,4 @@ d3.csv("walmartdata.csv").then(function(walData, err) {
         }
       }
     });
-}).catch(function(error) {
-  console.log(error);
-});
+} 
