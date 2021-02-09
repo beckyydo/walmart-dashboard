ddf4c2ce2339364ee8f06d890b937368e953751d
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-02-08 19:15:08 -0500

    update

--- actualApp/static/js/app.js
+++ actualApp/static/js/app.js
@@ -273,3 +273,94 @@ function updatePlotly(){
     })
 };
     
+// ******************************** STORE LOCATION ********************************
+var link = '/data/walmartLocations.geojson'
+
+fetch(link).then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    createFeatures(data.features);
+});
+
+function createFeatures(locationData) {
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.name + "</h3><hr/><h5>" + feature.properties.address1 + "</h5>");
+    }
+
+    var myIcon = L.icon({
+        iconUrl: '/data/cart_blue.png',
+        iconSize: [30,20]
+    })
+
+    var stores = L.geoJSON(locationData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function(feature, latlng) {
+            var marker = L.marker(latlng, {icon: myIcon});
+            marker.bindPopup(feature.properties.name + "<br/>" + feature.properties.address1);
+            return marker;
+        }
+    });
+    
+    createMap(stores);
+
+}
+
+function createMap(stores) {
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+    });
+
+    var streetmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+    });
+
+    var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+    });    
+
+    var baseMaps = {
+        "Dark Map": darkmap,
+        "Street Map": streetmap,
+        "Light Map": lightmap
+        
+    };
+
+    var overlayMaps = {
+        Stores: stores
+    };
+
+    var myMap = L.map("map", {
+        center: [36.358498566, -94.209832494],
+        zoom: 5,
+        layers: [streetmap, stores]
+    });
+
+    var clusters = L.markerClusterGroup();
+    clusters.addLayer(stores);
+    
+    myMap.addLayer(clusters);
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: true
+    }).addTo(myMap);
+
+}
+
+
+
