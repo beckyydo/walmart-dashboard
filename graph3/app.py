42ffd55313f3d883ef2581c7292dbc7fb443d7e6
lizsalasmtz <70294803+lizsalasmtz@users.noreply.github.com>
2021-02-11 12:12:21 -0500

    using elephant SQL

--- graph3/app.py
+++ graph3/app.py
@@ -5,42 +5,17 @@ from flask import (
     jsonify,
     request,
     redirect)
-# from flask_sqlalchemy import SQLAlchemy
 import psycopg2
-
-# import sqlalchemy
-# from sqlalchemy import create_engine
-# from sqlalchemy import func
-# from sqlalchemy.ext.automap import automap_base
-# from sqlalchemy.orm import session
-
 
 app = Flask(__name__)
 
 @app.route('/data')
 def send_data():
-    # using psycopg2...
-    # local test database
-    conn = psycopg2.connect(host='localhost', port='5432', dbname='test', user='postgres', password='postgres')
-
-    # team elephantSQL
-    # conn = psycopg2.connect("host='otto.db.elephantsql.com' dbname='ofiglsqd' user='ofiglsqd' password='vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe'")  
+    conn = psycopg2.connect(host='otto.db.elephantsql.com', port='5432', dbname='ofiglsqd', user='ofiglsqd', password='vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe')
 
     cur = conn.cursor()
-    cur.execute("select year, month, sales::float from public.sales_by_period_by_year;")
-    
-    # row_headers=[x[0] for x in cur.description]
-
-    # using SQLAlchemy
-    # engine = create_engine("postgres://ofiglsqd:vVojrG9_zzJZCOLXz8rhKWXk6ivvYqAe@otto.db.elephantsql.com:5432/ofiglsqd", echo=False)
-    # Base = automap_base()
-    # Base.prepare(engine, reflect=True)
-
-    # graph3 = Base.classes.graph3
-    # session = Session(engine)
-
-    # tmp_data = [col for col in cur]
-    
+    cur.execute("select year, month, sales::float from public.sales_by_period;")
+        
     tmp_data = cur.fetchall()
     payload = []
     content = {}
